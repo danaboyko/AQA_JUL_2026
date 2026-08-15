4d1e5b0a0a3da6f68cf36cf79a5622dea57b46ca
Bogdana <dana_boyko@ukr.net>
2026-08-15 16:06:15 +0300

    HW4

--- src/org/prog/session5/Plane.java
+++ src/org/prog/session5/Plane.java
@@ -1,22 +1,17 @@
 package org.prog.session5;
 
 public class Plane {
-    public int seats;
-    public int board;
-    public void goTo(){
-        if(board <= seats){
+    public int seats = 3;
+    public int board = 4;
 
-           System.out.println( seats + "seats" + board + "boarded passengers");
-        } else{
+    public void goTo() {
+        if (board <= seats) {
+
+            System.out.println(seats + "seats" + board + "boarded passengers");
+        } else {
             System.out.println("too many passengers");
         }
     }
-    public static void main(String[] args) {
-    Plane plane = new Plane();
-
-    plane.seats = 3;
-    plane.board = 1;
-
-    plane.goTo();
- }
 }
+
+
