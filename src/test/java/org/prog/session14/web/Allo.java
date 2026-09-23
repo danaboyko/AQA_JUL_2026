0d2da3676c2cc2ac4901df3feaf620781c1cc5f0
Bogdana <dana_boyko@ukr.net>
2026-09-23 16:16:51 +0300

    HW11

--- src/test/java/org/prog/session14/web/Allo.java
+++ src/test/java/org/prog/session14/web/Allo.java
@@ -3,11 +3,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+
+import java.time.Duration;
 
 public class Allo {
 
@@ -30,12 +35,26 @@ public class Allo {
     public void WebTestAllo() {
 
        WebDriver driver = new ChromeDriver();
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         driver.manage().window().maximize();
         driver.get("https://allo.ua/");
         WebElement searchForm = driver.findElement(By.id("search-form__input"));
-        searchForm.sendKeys("IPhone");
+        searchForm.sendKeys("IPhone 16");
         searchForm.sendKeys(Keys.ENTER);
+
+         WebElement pictures = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product-card__pictures")));
+
+         Actions actions = new Actions(driver);
+         actions.moveToElement(pictures);
+         actions.click();
+         actions.perform();
+         WebElement codeElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("p-view__header-content")));
+         WebElement priceElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("v-pb")));
+
+         System.out.println("Код товару:" + codeElement.getText());
+         System.out.println("Ціна товару:"+ priceElement.getText());
+
 
     }
 }
